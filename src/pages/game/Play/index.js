d7c6a036ce9faf39c7d0f470ac124a26d3e39276
lopezrunco <lopezrunco@gmail.com>
2021-12-30 13:09:20 -0300

    Codigo base comun para SinglePlayer & MultiPlayer

--- src/pages/game/Play/index.js
+++ src/pages/game/Play/index.js
@@ -1,7 +1,25 @@
+import { useState } from "react"
 import { useParams } from "react-router-dom"
+
+// Elementos del juego
+const choices = [
+    { "id": 1, "name": "rock", "losesTo": 2 },
+    { "id": 2, "name": "paper", "losesTo": 3 },
+    { "id": 3, "name": "scissors", "losesTo": 1 }
+]
 
 function Play() {
     const { id } = useParams()
+    const [userMove, setUserMove] = useState(null)
+
+    function handleUserChoice(choice) {
+        const chosenChoise = choices.find(choiceItem => choiceItem.id === choice)
+        setUserMove(chosenChoise)
+
+        console.log(chosenChoise)
+    }
+
+    // TODO: llamar al endpoint y enviar la eleccion
 
     return (
         <main>
@@ -12,7 +30,23 @@ function Play() {
                     </div>
 
                     <div className='col-12'>
-
+                        <div className="row">
+                            <div className="col-4">
+                                <button className="rock" onClick={() => handleUserChoice(1)}>
+                                    ROCK
+                                </button>
+                            </div>
+                            <div className="col-4">
+                                <button className="paper" onClick={() => handleUserChoice(2)}>
+                                    PAPER
+                                </button>
+                            </div>
+                            <div className="col-4">
+                                <button className="scissors" onClick={() => handleUserChoice(3)}>
+                                    SCISSORS
+                                </button>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
