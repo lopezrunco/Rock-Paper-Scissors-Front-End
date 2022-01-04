0119f64a1c2d80688d326ea3aff35b1ea72276b5
lopezrunco <lopezrunco@gmail.com>
2022-01-03 21:54:07 -0300

    Ocultar ultima jugada cuando un usuario va adelante

--- src/pages/game/OnPlayGames/components/OnPlayGame/index.js
+++ src/pages/game/OnPlayGames/components/OnPlayGame/index.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom'
 import rockImg from '../../../../../assets/img/rock.png'
 import paperImg from '../../../../../assets/img/paper.png'
 import scissorsImg from '../../../../../assets/img/scissors.png'
+import questionImg from '../../../../../assets/img/question.png'
 
 import './style.scss'
+
 
 function OnPlayGame({ game }) {
 
@@ -41,6 +43,19 @@ function OnPlayGame({ game }) {
             movesPlayerTwo.push(<h5>{element}</h5>)
         }
     })
+
+    const questionImgTag = <img className='question-img' src={questionImg} alt='Question' />
+
+    // Chequea que jugador va adelante del otro y oculta su jugada
+    if (movesPlayerOne.length > movesPlayerTwo.length) {        // Jugador 1 va delante del jugador 2
+        movesPlayerOne.splice(-1, 1, questionImgTag)
+    } else if (movesPlayerOne.length < movesPlayerTwo.length) { // Jugador 2 va delante del jugador 1
+        movesPlayerTwo.splice(-1, 1, questionImgTag)
+    } else {
+        console.log('Largos iguales')
+    }
+
+    console.log('arr', movesPlayerTwo)
 
     return (
         <div className="col-md-4 onplay-game-wrapper">
